e52290acfc07c373fe155ca05a169a549f26ef34
user.email <sreekanthbalireddi@gmail.com>
2024-07-04 12:35:24 +0530

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-console.log('====================================');
-console.log("Connected");
-console.log('====================================');
+document.addEventListener("DOMContentLoaded", function() {
+    var clickableImage = document.getElementById("ClickableImage");
+    var message = document.getElementById("message");
+
+    clickableImage.addEventListener("click", function() {
+        // Perform your add-to-cart functionality here
+
+        // Show the message
+        message.style.display = "block";
+
+        // Hide the message after 3 seconds (optional)
+        //setTimeout(function() {
+           // message.style.display = "none";
+        //}, 90000);
+    });
+});
